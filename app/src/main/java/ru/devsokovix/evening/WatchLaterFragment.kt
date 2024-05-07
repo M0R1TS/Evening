5ad4f4b40348f893da8b6ae336f18630ba920785
morits <mor1ts@mail.ru>
2024-05-07 20:32:08 +0300

    Home work module 29: 1. Убрал Scene в фрагментах 2. Создал два новых фрагмента 3. Создал фоновые изображения 4. Добавил анимации к фрагментам

---
+++ app/src/main/java/ru/devsokovix/evening/WatchLaterFragment.kt
@@ -0,0 +1,26 @@
+package ru.devsokovix.evening
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.devsokovix.evening.databinding.FragmentWatchLaterBinding
+
+
+class WatchLaterFragment : Fragment() {
+    private lateinit var binding: FragmentWatchLaterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWatchLaterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        AnimationHelper.performFragmentCircularRevealAnimation(binding.watchLaterFragmentRoot, requireActivity(), 3)
+    }
+}
