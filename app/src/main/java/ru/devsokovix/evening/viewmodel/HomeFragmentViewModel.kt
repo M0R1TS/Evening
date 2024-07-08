47306d2b2e29c4e6ffa35794cd591efb775b9455
morits <mor1ts@mail.ru>
2024-07-08 22:12:55 +0300

    Home work module 36.1: 1. Основное задание сделано, через библиотеку Dagger

--- app/src/main/java/ru/devsokovix/evening/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/ru/devsokovix/evening/viewmodel/HomeFragmentViewModel.kt
@@ -6,55 +6,26 @@ import androidx.recyclerview.widget.RecyclerView
 import ru.devsokovix.evening.App
 import ru.devsokovix.evening.data.Interactor
 import ru.devsokovix.evening.domain.Film
+import javax.inject.Inject
 
 class HomeFragmentViewModel : ViewModel() {
-    val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
-    private var page = 1
-
-    // Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
-
+    //Инициализируем интерактор
+    @Inject
+    lateinit var interactor: Interactor
+    val filmsListLiveData = MutableLiveData<List<Film>>()
     init {
-        getdata(1)
+        App.instance.dagger.inject(this)
+        interactor.getFilmsFromApi(1, object : ApiCallback {
+            override fun onSuccess(films: List<Film>) {
+                filmsListLiveData.postValue(films)
+            }
+
+            override fun onFailure() {
+            }
+        })
     }
-
-    private fun getdata(page: Int) {
-        interactor.getFilmsFromApi(
-            page,
-            object : ApiCallback {
-                override fun onSuccess(films: List<Film>) {
-                    filmsListLiveData.postValue(films)
-                }
-
-                override fun onFailure() {
-                }
-            },
-        )
+    interface ApiCallback {
+        fun onSuccess(films: List<Film>)
+        fun onFailure()
     }
-
-    fun doPagination(
-        visibleItemCount: Int,
-        totalItemCount: Int,
-        postVisibleItemCount: Int,
-        recyclerView: RecyclerView
-    ) {
-        if ((visibleItemCount + postVisibleItemCount) >= totalItemCount) {
-            recyclerView.postDelayed(
-                Runnable {
-                    // перемещаем пользователя на вверх
-                    recyclerView.smoothScrollToPosition(0)
-
-                    // задержка отсутствует
-                },
-                0,
-            )
-            getdata(++page)
-        }
-    }
-}
-
-interface ApiCallback {
-    fun onSuccess(films: List<Film>)
-
-    fun onFailure()
 }
