fbcb0add0f8773570fa72b704db072f1897ddff9
morits <mor1ts@mail.ru>
2024-07-20 18:21:30 +0300

    Home work module 39.1: 1. Добавьте в приложение БД, куда будут сохраняться фильмы 2. Создайте дополнительные методы для обновления и удаления данных из БД, а также выриант запросов фильмов с определённым рейтингом

--- app/src/main/java/ru/devsokovix/evening/data/Interactor.kt
+++ app/src/main/java/ru/devsokovix/evening/data/Interactor.kt
@@ -3,6 +3,7 @@ package ru.devsokovix.evening.data
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import ru.devsokovix.evening.domain.Film
 import ru.devsokovix.evening.domain.TmdbResultsDto
 import ru.devsokovix.evening.mn_interface.TmdbApi
 import ru.devsokovix.evening.utils.API
@@ -14,21 +15,32 @@ class Interactor(
     private val retrofitService: TmdbApi,
     private val preferences: PreferenceProvider,
 ) {
+    // В конструктор мы будем передавать коллбэк из вью модели, чтобы реагировать на то, когда фильмы будут получены
+    // и страницу, которую нужно загрузить (это для пагинации)
     fun getFilmsFromApi(
         page: Int,
         callback: HomeFragmentViewModel.ApiCallback,
     ) {
         // Метод getDefaultCategoryFromPreferences() будет нам получать при каждом запросе нужный нам список фильмов
         retrofitService
-            .getFilms(getDefaultCategoryFromPreferences(), API.KEY, "ru-RU", page)
-            .enqueue(
+            .getFilms(
+                getDefaultCategoryFromPreferences(),
+                API.KEY,
+                "ru-RU",
+                page,
+            ).enqueue(
                 object : Callback<TmdbResultsDto> {
                     override fun onResponse(
                         call: Call<TmdbResultsDto>,
                         response: Response<TmdbResultsDto>,
                     ) {
                         // При успехе мы вызываем метод передаем onSuccess и в этот коллбэк список фильмов
-                        callback.onSuccess(Converter.convertApiListToDtoList(response.body()?.tmdbFilms))
+                        val list = Converter.convertApiListToDtoList(response.body()?.tmdbFilms)
+                        // Кладем фильмы в бд
+                        list.forEach {
+                            repo.putToDb(film = it)
+                        }
+                        callback.onSuccess(list)
                     }
 
                     override fun onFailure(
@@ -49,4 +61,10 @@ class Interactor(
 
     // Метод для получения настроек
     fun getDefaultCategoryFromPreferences() = preferences.getDefaultCategory()
+
+    fun getFilmsFromDB(): List<Film> = repo.getAllFromDB()
+
+    fun clearCache() = repo.clearCache()
+
+    fun clearInCacheBadFilms() = repo.clearInCacheBadFilms()
 }
--- app/src/main/java/ru/devsokovix/evening/di/modules/DatabaseModule.kt
+++ app/src/main/java/ru/devsokovix/evening/di/modules/DatabaseModule.kt
@@ -1,13 +1,19 @@
 package ru.devsokovix.evening.di.modules
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import ru.devsokovix.evening.data.MainRepository
+import ru.devsokovix.evening.data.db.DatabaseHelper
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabaseHelper(context: Context) = DatabaseHelper(context)
+
     @Provides
     @Singleton
-    fun provideRepository() = MainRepository()
+    fun provideRepository(databaseHelper: DatabaseHelper) = MainRepository(databaseHelper)
 }
