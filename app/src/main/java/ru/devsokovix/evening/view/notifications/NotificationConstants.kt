60009f3776b44d8621466ddc1752b26b5694b4a6
morits <mor1ts@mail.ru>
2024-10-05 17:40:26 +0300

    Home work module 49: 1. При нажатии на кпопку «Посмотреть позже» выдаёт поле с датой и временем , в которое надо напомнить о фильме, и в это время приходит нотификация.

--- app/src/main/java/ru/devsokovix/evening/view/notifications/NotificationConstants.kt
+++ app/src/main/java/ru/devsokovix/evening/view/notifications/NotificationConstants.kt
@@ -3,4 +3,6 @@ package ru.devsokovix.evening.view.notifications
 
 object NotificationConstants {
     const val CHANNEL_ID = "FilmsSearchChannel"
+    const val FILM_BUNDLE_KEY = "FILM_BUNDLE"
+    const val FILM_KEY = "FILM_KEY"
 }
